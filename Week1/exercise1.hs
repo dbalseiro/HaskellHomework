cbbbbc87aaad77b9476082eca375f63eb8ca99e9
Diego Balseiro <diego.balseiro@syntagma.com.ar>
2017-08-14 20:28:43 -0500

    styl corrections

--- Week1/exercise1.hs
+++ Week1/exercise1.hs
@@ -1,49 +1,58 @@
-toDigits :: Integer -> [Integer]
-toDigitsRev :: Integer -> [Integer]
-reverseList :: [Integer] -> [Integer]
-concatList :: [Integer] -> [Integer] -> [Integer]
+main :: IO ()
+main = do
+    display 4012888888881881;
+    display 4012888888881882
 
-doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOtherRev :: [Integer] -> [Integer]
+display :: Integer -> IO ()
+display i = 
+    if validate i
+        then putStrLn "OK"
+        else putStrLn "ERR"
 
-sumDigits :: [Integer] -> Integer
-sumIndividualDigits :: [Integer] -> Integer
-
+--validate CC number
+-- 
 validate :: Integer -> Bool
+validate i = sumDigits (doubleEveryOther (toDigits i)) `mod` 10 == 0
 
+-- sum all digits from a list
+sumDigits :: [Integer] -> Integer
+sumDigits [] = 0
+sumDigits (x:xs) = (sumIndividualDigits (toDigits x)) + (sumDigits xs)
+
+-- reverse toDigits
+toDigits :: Integer -> [Integer]
+toDigits n = reverseList (toDigitsRev n)
+
+-- sum a list of integers
+sumIndividualDigits :: [Integer] -> Integer
+sumIndividualDigits [] = 0
+sumIndividualDigits (x:xs) = x + (sumIndividualDigits xs)
+
+-- From units onward, add digits to an array
+toDigitsRev :: Integer -> [Integer]
 toDigitsRev n
     | n <= 0 = []
     | otherwise = n `mod` 10 : toDigitsRev (n `div` 10)
 
-toDigits n = reverseList (toDigitsRev n)
-
+--Reverse a list
+reverseList :: [a] -> [a]
 reverseList [] = []
 reverseList (x:xs) = concatList (reverseList xs) (x : [])
 
+-- concatenate two lists into one
+concatList :: [a] -> [a] -> [a]
 concatList [] y = y
 concatList (x:xs) y = x : (concatList xs y)
 
+-- double every odd element from a list
+doubleEveryOtherRev :: [Integer] -> [Integer]
 doubleEveryOtherRev [] = []
 doubleEveryOtherRev (x:[]) = [x]
 doubleEveryOtherRev (x:y:xs) = x : y * 2 : doubleEveryOtherRev(xs)
 
+-- double every od element from a list, but begining from the end.
+doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther x = reverseList (doubleEveryOtherRev (reverseList x))
 
-sumDigits [] = 0
-sumDigits (x:xs) = (sumIndividualDigits (toDigits x)) + (sumDigits xs)
 
-sumIndividualDigits [] = 0
-sumIndividualDigits (x:xs) = x + (sumIndividualDigits xs)
-
-validate i = sumDigits (doubleEveryOther (toDigits i)) `mod` 10 == 0
-
-display i = 
-    if validate i
-    then putStrLn "OK"
-    else putStrLn "ERR"
-
-
-main = do {
-    display 4012888888881881; display 4012888888881882
-}
 
--- Week1/exercise2.hs
+++ Week1/exercise2.hs
@@ -1,26 +1,25 @@
 type Peg = String
 type Move = (Peg, Peg)
-hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-performMove :: Integer -> Peg -> Peg -> Peg -> [Move]
-concatenateMoves :: [Move] -> [Move] -> [Move]
-display :: [Move] -> IO()
 
-concatenateMoves [] y = y
-concatenateMoves (x:xs) y = x : (concatenateMoves xs y)
-
-performMove 0 _ _ _ = []
-performMove n from to using = concatenateMoves
-    (concatenateMoves (performMove (n-1) from using to) [(from, to)])
-    (performMove (n-1) using to from)
-
-hanoi n a b c = performMove n a c b
-
-display [] = return()
-display (x:xs) = do {
-    putStrLn (fst x ++ "," ++ snd x);
-    display xs
-}
-
+main :: IO()
 main = display (hanoi 2 "a" "b" "c")
 
+display :: [Move] -> IO()
+display [] = return()
+display (x:xs) = do
+    putStrLn (fst x ++ "," ++ snd x);
+    display xs
 
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi n a b c = performMove n a c b
+
+performMove :: Integer -> Peg -> Peg -> Peg -> [Move]
+performMove 0 _ _ _ = []
+performMove n from to using = 
+    (performMove (n-1) from using to) ++
+    [(from, to)] ++
+    (performMove (n-1) using to from)
+
+concatenateMoves :: [Move] -> [Move] -> [Move]
+concatenateMoves [] y = y
+concatenateMoves (x:xs) y = x : (concatenateMoves xs y)
